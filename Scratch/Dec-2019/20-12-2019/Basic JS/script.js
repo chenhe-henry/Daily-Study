be5fe8c50b9f7f455db7527472a2920ce2bd9471
chenhe-henry <chenhe.elec@gmail.com>
2020-01-04 22:17:45 +1100

    add practice

---
+++ Scratch/Dec-2019/20-12-2019/Basic JS/script.js
@@ -0,0 +1,29 @@
+const greeting = "Hello Ketty";
+const restaurant = "McDonalds";
+console.log(greeting);
+console.log("-----------------------");
+
+const myAge = 35;
+const roughPi = 3.14;
+
+console.log(myAge);
+console.log("-----------------------");
+
+const catsAreBest = true;
+const dogsRule = false;
+
+console.log(catsAreBest);
+console.log(dogsRule);
+console.log("-----------------------");
+
+const myBiceps = null;
+console.log(myBiceps);
+console.log("-----------------------");
+console.log(4 + 2);
+console.log("Hello World");
+console.log("-----------------------");
+let courseName = "S" + "E" + "I";
+console.log(courseName);
+let test;
+console.log("-----------------------");
+console.log("-----------------------");
