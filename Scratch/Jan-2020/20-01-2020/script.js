0420f88293e9449c1c36150c590475aa7bc1a6e3
chenhe-henry <chenhe.elec@gmail.com>
2020-01-22 09:20:55 +1100

    update practice

---
+++ Scratch/Jan-2020/20-01-2020/script.js
@@ -0,0 +1,5 @@
+// document.getElementById("table").textContent = Math.floor(Math.random() * 10);
+
+// document.getElementById("tasks").textContent = Math.floor(Math.random() * 10);
+
+// // document.getElementById("reload-button").onclick(location.reload());
