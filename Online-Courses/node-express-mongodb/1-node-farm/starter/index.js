3926431961c8c4712fa430c8d50742bbb91bf718
chenhe-henry <chenhe.elec@gmail.com>
2020-01-22 22:46:55 +1100

    simple web server

--- Online-Courses/node-express-mongodb/1-node-farm/starter/index.js
+++ Online-Courses/node-express-mongodb/1-node-farm/starter/index.js
@@ -1,6 +1,10 @@
 // fs is a module, named File System
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
+///////////////////////////////////////////////
+//Files
 //Blocking, synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
@@ -11,16 +15,40 @@ const fs = require("fs");
 // console.log("File written!");
 
 //Non-blocking, asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR!");
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", err => {
-        console.log("Your file has been written ");
-      });
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", err => {
+//         console.log("Your file has been written ");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file");
+
+///////////////////////////////////////////////
+//Server
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW!");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT!");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world"
     });
-  });
+    res.end("<h1>Page not found!</h1>");
+  }
 });
-console.log("Will read file");
+// 127.0.0.1 is the localhost IP address
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
