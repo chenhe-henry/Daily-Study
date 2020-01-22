97799273fc9f31e3ea598e089a279e31b0fc11c7
chenhe-henry <chenhe.elec@gmail.com>
2020-01-22 23:03:18 +1100

    create simple api

--- Online-Courses/node-express-mongodb/1-node-farm/starter/index.js
+++ Online-Courses/node-express-mongodb/1-node-farm/starter/index.js
@@ -33,6 +33,10 @@ const url = require("url");
 
 ///////////////////////////////////////////////
 //Server
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -40,6 +44,11 @@ const server = http.createServer((req, res) => {
     res.end("This is the OVERVIEW!");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT!");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json"
+    });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
