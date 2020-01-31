b99d83449d95c60b820c66b1ae83c6a4b5a61228
chenhe-henry <chenhe.elec@gmail.com>
2020-02-01 00:15:08 +1100

    add functional component

--- Online-Courses/React/good-choice/src/App.js
+++ Online-Courses/React/good-choice/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./App.scss";
+import "./scss/App.scss";
+import Person from "./Person/Person";
 
 class App extends React.Component {
   state = {};
@@ -8,6 +9,8 @@ class App extends React.Component {
       <div className="App">
         {/* todo: add more content and component here */}
         <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person />
       </div>
     );
   }
