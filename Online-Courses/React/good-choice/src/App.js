e15c57871950f6ebe07f3014785ea8b84ea57c8f
chenhe-henry <chenhe.elec@gmail.com>
2020-01-31 16:26:10 +1100

    react practice and apply scss on react

---
+++ Online-Courses/React/good-choice/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.scss";
+
+class App extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
