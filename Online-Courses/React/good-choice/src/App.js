69edc8a86869c0dc55c35e024095ce72cbc12222
chenhe-henry <chenhe.elec@gmail.com>
2020-02-03 20:52:15 +1100

    use map

--- Online-Courses/React/good-choice/src/App.js
+++ Online-Courses/React/good-choice/src/App.js
@@ -55,22 +55,9 @@ class App extends React.Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.togglePersonsHandler.bind(this, "Max!")}
-            changed={this.nameChangedHandler}
-          >
-            My Hobbies: Racing
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} />;
+          })}
         </div>
       );
     }
