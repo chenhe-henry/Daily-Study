8c01e3538315b7b2203f22d0dda9eccc115461be
chenhe-henry <chenhe.elec@gmail.com>
2020-03-01 21:17:32 +1100

    ui interaction with css clip path

---
+++ Scratch/Feb-2020/27-02-2020/threekingdoms.js
@@ -0,0 +1,13 @@
+const rate = {
+  man1: "32.55",
+  man2: "32.55",
+  man3: "14.2",
+  man4: "14.2",
+  man5: "6.5"
+};
+
+const sumRate = arr => {
+  arr.forEach(e => console.log(e));
+};
+
+sumRate(rate);
