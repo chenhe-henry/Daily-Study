d85465d69a72a4d90199bfe2da0306d4d319493b
chenhe-henry <chenhe.elec@gmail.com>
2020-04-30 09:43:17 +1000

    use console.time and console.timeEnd to calculate function's run time

---
+++ ConceptLearning/Tricky/calculateRunTime.js
@@ -0,0 +1,7 @@
+console.time("loop");
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
+console.timeEnd("loop");
+
+//use console.time('name') and console.timeEnd('name') to calculate the function's runtime
