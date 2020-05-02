82e37b458bbe9140911d210df7118bdd1038cf3e
chenhe-henry <chenhe.elec@gmail.com>
2020-05-02 10:50:25 +1000

    window.location  property and method

---
+++ Scratch/May-2020/02-05-2020/script.js
@@ -0,0 +1,9 @@
+var test = document.getElementById("testOne");
+
+test.addEventListener("click", () => {
+  //   window.location.assign(
+  //     "https://www.youtube.com/user/DigitalOceanVideos/playlists"
+  //   );
+  console.log("clicked");
+  alert(window.location.host);
+});
