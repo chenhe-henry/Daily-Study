f53e43bc20354d42553c6970fea1746a78884290
chenhe-henry <chenhe.elec@gmail.com>
2020-05-02 12:32:09 +1000

    get to know the difference between <a href> and window.location

--- Scratch/May-2020/02-05-2020/script.js
+++ Scratch/May-2020/02-05-2020/script.js
@@ -1,10 +1,14 @@
 var test = document.getElementById("testOne");
 
 test.addEventListener("click", () => {
-  //   window.location.assign(
-  //     "https://www.youtube.com/user/DigitalOceanVideos/playlists"
-  //   );
-  document.getElementById("testResult").innerHTML =
-    "This page is host on" + window.location.host;
-  console.log("clicked");
+  const redirect = () => {
+    window.location.assign(
+      "https://www.youtube.com/user/DigitalOceanVideos/playlists"
+    );
+  };
+  setTimeout(redirect, 10000);
+
+  //   document.getElementById("testResult").innerHTML =
+  //     "This page is host on " + window.location.href;
+  //   console.log("clicked");
 });
