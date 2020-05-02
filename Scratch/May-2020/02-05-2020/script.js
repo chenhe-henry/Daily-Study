ad5de6633056ab2f7787f49c7c6f1d0c1c88dec3
chenhe-henry <chenhe.elec@gmail.com>
2020-05-02 12:04:16 +1000

    innerHTML

--- Scratch/May-2020/02-05-2020/script.js
+++ Scratch/May-2020/02-05-2020/script.js
@@ -4,6 +4,7 @@ test.addEventListener("click", () => {
   //   window.location.assign(
   //     "https://www.youtube.com/user/DigitalOceanVideos/playlists"
   //   );
+  document.getElementById("testResult").innerHTML =
+    "This page is host on" + window.location.host;
   console.log("clicked");
-  alert(window.location.host);
 });
