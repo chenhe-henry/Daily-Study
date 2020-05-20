072672debe99863489c9bc9abd9357864007ac81
chenhe-henry <chenhe.elec@gmail.com>
2020-05-20 17:47:24 +1000

    quiz constructor, with random question and answers

--- Code-Challenge/QuizGameInTheConsole/QuizGameInTheConsole.js
+++ Code-Challenge/QuizGameInTheConsole/QuizGameInTheConsole.js
@@ -38,3 +38,52 @@ the power of closures for this, but you don't have to, just do this with the too
 
 11. Display the score in the console. Use yet another method for this.
 */
+
+function Question(question, answers, correct) {
+  this.question = question;
+  this.answers = answers;
+  this.correct = correct;
+}
+
+Question.prototype.displayQuestion = function () {
+  console.log(this.question);
+
+  for (var i = 0; i < this.answers.length; i++) {
+    console.log(i + ": " + this.answers[i]);
+  }
+};
+
+Question.prototype.checkAnswer = function (ans) {
+  if (ans === this.correct) {
+    console.log("Correct answer!");
+  } else {
+    console.log("Wrong answer.");
+  }
+};
+var q1 = new Question(
+  "Is JavaScript the coolest programming language in the world?",
+  ["Yes", "No"],
+  0
+);
+
+var q2 = new Question(
+  "What is the name of this course's teacher",
+  ["John", "Micheal", "Jonas"],
+  2
+);
+
+var q3 = new Question(
+  "what does best describe coding?",
+  ["Boring", "Hard", "Fun", "Tedius"],
+  2
+);
+
+var questions = [q1, q2, q3];
+
+var n = Math.floor(Math.random() * questions.length);
+
+questions[n].displayQuestion();
+
+var answer = parseInt(prompt("Please select the correct answer."));
+
+questions[n].checkAnswer(answer);
