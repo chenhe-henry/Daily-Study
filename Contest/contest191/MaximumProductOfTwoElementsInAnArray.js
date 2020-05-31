55e2becdb3926a31ef330ad63207565c98971698
chenhe-henry <chenhe.elec@gmail.com>
2020-05-31 12:46:24 +1000

    contest 191 #1 done

---
+++ Contest/contest191/MaximumProductOfTwoElementsInAnArray.js
@@ -0,0 +1,36 @@
+// Given the array of integers nums, you will choose two different indices i and j of that array.
+// Return the maximum value of(nums[i] - 1) * (nums[j] - 1).
+
+//     Example 1:
+
+// Input: nums = [3, 4, 5, 2]
+// Output: 12
+// Explanation: If you choose the indices i = 1 and j = 2(indexed from 0),
+// you will get the maximum value, that is, (nums[1] - 1) * (nums[2] - 1) = (4 - 1) * (5 - 1) = 3 * 4 = 12.
+// Example 2:
+
+// Input: nums = [1, 5, 4, 5]
+// Output: 16
+// Explanation: Choosing the indices i = 1 and j = 3(indexed from 0), you will get the maximum value of(5 - 1) * (5 - 1) = 16.
+// Example 3:
+
+// Input: nums = [3, 7]
+// Output: 12
+
+// Constraints:
+
+// 2 <= nums.length <= 500
+// 1 <= nums[i] <= 10 ^ 3
+var maxProduct = function (nums) {
+  let multiplyArr = [];
+  let IndexOfLargestNum = nums.indexOf(Math.max(...nums));
+  multiplyArr.push(nums.splice(IndexOfLargestNum, 1));
+  let IndexOfSecondLargestNum = nums.indexOf(Math.max(...nums));
+  multiplyArr.push(nums.splice(IndexOfSecondLargestNum, 1));
+  let result = (multiplyArr[0] - 1) * (multiplyArr[1] - 1);
+  return result;
+};
+console.log(maxProduct([3, 4, 5, 2]));
+console.log(maxProduct([1, 5, 4, 5]));
+console.log(maxProduct([3, 7]));
+console.log(maxProduct([10, 2, 5, 2]));
