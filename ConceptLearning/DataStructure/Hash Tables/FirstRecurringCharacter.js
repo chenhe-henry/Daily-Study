34f054f00d0ca287396ca8109e25e307d9243b22
chenhe-henry <chenhe.elec@gmail.com>
2020-06-13 01:33:53 +1000

    first recurring char

---
+++ ConceptLearning/DataStructure/Hash Tables/FirstRecurringCharacter.js
@@ -0,0 +1,40 @@
+//Given an array = [2,5,1,2,3,5,1,2,4]
+// It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]
+// It should return 1
+
+//Given an array = [2,3,4,5]
+// It should return undefined
+
+// const firstRecurringCharacter = (input) => {
+//   for (let i = 0; i < input.length; i++) {
+//     for (let j = i + 1; j < input.length; j++) {
+//       if (input[i] === input[j]) {
+//         return input[i];
+//       }
+//     }
+//   }
+//   return undefined;
+// }; // O(n^2)
+
+// console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]));
+// console.log(firstRecurringCharacter([2, 1, 1, 2, 3, 5, 1, 2, 4]));
+// console.log(firstRecurringCharacter([2, 3, 4, 5]));
+
+const firstRecurringCharacter2 = (input) => {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]] !== undefined) {
+      return input[i];
+    } else {
+      map[input[i]] = i;
+    }
+    console.log(map);
+  }
+
+  return undefined;
+};
+console.log(firstRecurringCharacter2([2, 5, 5, 2, 3, 5, 1, 2, 4]));
+console.log(firstRecurringCharacter2([2, 1, 1, 2, 3, 5, 1, 2, 4]));
+console.log(firstRecurringCharacter2([2, 3, 4, 5]));
