b1171e5a09db0869312c2d419985dac01e9872c0
chenhe-henry <chenhe.elec@gmail.com>
2020-06-14 12:42:56 +1000

    contest 193 # 1 done

---
+++ Contest/contest193/Running Sum of 1d Array.js
@@ -0,0 +1,35 @@
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+// Example 2:
+
+// Input: nums = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+// Example 3:
+
+// Input: nums = [3,1,2,10,1]
+// Output: [3,4,6,16,17]
+
+// Constraints:
+
+// 1 <= nums.length <= 1000
+// -10^6 <= nums[i] <= 10^6
+var runningSum = function (nums) {
+  let newSumArr = [];
+  let tempSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    tempSum += nums[i];
+    newSumArr.push(tempSum);
+  }
+  return newSumArr;
+};
+console.log(runningSum([1, 2, 3, 4]));
+console.log(runningSum([1, 1, 1, 1, 1]));
+console.log(runningSum([3, 1, 2, 10, 1]));
