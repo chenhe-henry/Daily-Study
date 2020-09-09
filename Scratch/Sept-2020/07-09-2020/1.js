28c45e7eb8f035cdf0e84ffb65b0b46226f50d88
chenhe-henry <chenhe.elec@gmail.com>
2020-09-09 18:07:42 +1000

    update

---
+++ Scratch/Sept-2020/07-09-2020/1.js
@@ -0,0 +1,33 @@
+Delivered-To: noreply@skilligi.com
+Return-Path: <noreply@skilligi.com>
+ARC-Seal: i=1; a=rsa-sha256; t=1599633255; cv=none;
+    d=zohomail.in; s=zohoarc;
+    b=DBxZHR0q8XDVIo37mW9NTP7QKp67tBONhlLNOoJzRa/rPZ97YLEbsKCrKpZM8SNsXDC9Q2dMFMZ/kvXkO826rjodCN72+EYkSD2fzwECBNt8i5N/VTSFikRMEJEE9IJD1QKxkjHOUtL/I1UD97/fQbY4qgJw0q+/KS2xxojMjhY=
+ARC-Message-Signature: i=1; a=rsa-sha256; c=relaxed/relaxed; d=zohomail.in; s=zohoarc;
+    t=1599633255; h=Content-Type:Content-Transfer-Encoding:Date:From:MIME-Version:Message-ID:Subject:To;
+    bh=jK32ciPzi0FPLsd5LG4ogf2sPCHsxG9av8Ukxc+Wk6A=;
+    b=fhy91xPAs1p9rv94+bCMJ/pozEIr0FOeOrSmNW/D2lgwoth78uoabgTpLAZiVFHFAjd1M1pQUzwb9uzzRcVkRNLj+FJKsFlDhnpLonVrLJRe9Rm02W2uyBcGYse2eiEd6JDj2sYZYpyl4LUTt+8v2+kJuz949MlUJ/LP49450u0=
+ARC-Authentication-Results: i=1; mx.zohomail.in;
+    dkim=pass header.i=skilligi.com;
+    spf=pass smtp.mailfrom=noreply@skilligi.com;
+    dmarc=pass header.from=<noreply@skilligi.com> header.from=<noreply@skilligi.com>
+DKIM-Signature: v=1; a=rsa-sha256; q=dns/txt; c=relaxed/relaxed; t=1599633255;
+    s=zoho; d=skilligi.com; i=noreply@skilligi.com;
+    h=Date:From:To:Message-ID:Subject:Mime-Version:Content-Type:Content-Transfer-Encoding;
+    bh=jK32ciPzi0FPLsd5LG4ogf2sPCHsxG9av8Ukxc+Wk6A=;
+    b=LD3D0/i5UVdxfkeeLb7BK+QfH5nfEj96Qk7/AeM8BzP+/lalyU91hC7yPDug68nO
+    9PjDH6dMmAdBoIW3J0h03CJI0/jfWl20PCr9mTfEaGCszaGa9L7bdVNbXM49vsJRb/l
+    rBrDTjV6evDPgX9NNwsIsoVWPoXc7NGz09hnZj8g=
+Received: from skilligi.com (157.230.38.38 [157.230.38.38]) by mx.zoho.in
+    with SMTPS id 1599633252459581.540642600895; Wed, 9 Sep 2020 12:04:12 +0530 (IST)
+Date: Wed, 09 Sep 2020 06:34:10 +0000
+From: Skilligi <noreply@skilligi.com>
+To: noreply@skilligi.com
+Message-ID: <5f5877624b869_749d2ad328b3f968951ab@skilligi.mail>
+Subject: New member in Skilligi
+Mime-Version: 1.0
+Content-Type: multipart/alternative;
+boundary="--==_mimepart_5f5877624b43f_749d2ad328b3f9689509e";
+charset=UTF-8
+Content-Transfer-Encoding: 7bit
+X-ZohoMailClient: External
---
+++ Scratch/Sept-2020/07-09-2020/2.js
@@ -0,0 +1,33 @@
+Delivered-To: noreply@skilligi.com
+Return-Path: <noreply@skilligi.com>
+ARC-Seal: i=1; a=rsa-sha256; t=1599633255; cv=none;
+    d=zohomail.in; s=zohoarc;
+    b=Si7DZHPi7hvrCyo2v9V3LK4sC9uLvxksSZX8xU0KX6tPShBo/9LEepBeCfSFu4jVOOjEjt7wdEtI+HAI4pLnDR6bLJRy3ZA70kr0QVaS8ntmL3s8AQB4oUo6rHpDxgdDNVaWsd8ZRMQhsE8I/c0qtEccwrWDxOAGvIOXqsw5W6M=
+ARC-Message-Signature: i=1; a=rsa-sha256; c=relaxed/relaxed; d=zohomail.in; s=zohoarc;
+    t=1599633255; h=Content-Type:Content-Transfer-Encoding:Date:From:MIME-Version:Message-ID:Subject:To;
+    bh=DqmMR/tM6asqHTFLjfnDUzqmzROE4UMqLavdT8Zhhng=;
+    b=I/gOt2H+inyvTlJyW59286BBweCJ67nQEkUotQ/sDO4quQQnrtyqJ0R87jscNfeUYWL0972mra8KJPJVJPjIfnOynGYQ6tX1Gg68sEagZ6+9NHHljKLCQK/Cb9ziRL/f2Fn6h5Iut5M34FjTh1g0ANyvB03cpkaE2RWnwZNCl6w=
+ARC-Authentication-Results: i=1; mx.zohomail.in;
+    dkim=pass header.i=skilligi.com;
+    spf=pass smtp.mailfrom=noreply@skilligi.com;
+    dmarc=pass header.from=<noreply@skilligi.com> header.from=<noreply@skilligi.com>
+DKIM-Signature: v=1; a=rsa-sha256; q=dns/txt; c=relaxed/relaxed; t=1599633255;
+    s=zoho; d=skilligi.com; i=noreply@skilligi.com;
+    h=Date:From:To:Message-ID:Subject:Mime-Version:Content-Type:Content-Transfer-Encoding;
+    bh=DqmMR/tM6asqHTFLjfnDUzqmzROE4UMqLavdT8Zhhng=;
+    b=Xep1HJLPhMbEmlP0gBLrsMdhCXb3iCOCbLm9UEM8W4DFCLxndbRlMQNW0vFCFr0u
+    +S/B8xGA927fSAmH7RwvrKMoOmQh71gY4HTLE3usblonDmD/ErOvKKMhyjxj9A3HpT5
+    wU683ECYC75DuKmxwubISkdS0GqV2HViAlW8+XfA=
+Received: from skilligi.com (157.230.38.38 [157.230.38.38]) by mx.zoho.in
+    with SMTPS id 1599633254144340.517601624656; Wed, 9 Sep 2020 12:04:14 +0530 (IST)
+Date: Wed, 09 Sep 2020 06:34:12 +0000
+From: Skilligi <noreply@skilligi.com>
+To: noreply@skilligi.com
+Message-ID: <5f587764c3ce_749d2ad328b3f96895495@skilligi.mail>
+Subject: New member in Skilligi
+Mime-Version: 1.0
+Content-Type: multipart/alternative;
+boundary="--==_mimepart_5f587764bf7c_749d2ad328b3f96895374";
+charset=UTF-8
+Content-Transfer-Encoding: 7bit
+X-ZohoMailClient: External
